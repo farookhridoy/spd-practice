e96ac7378c41f1d174602fce47c6ba1b8d028055
gmfaruk <gmfaruk2021@gmail.com>
2023-02-12 23:13:28 +0600

    Linked list add and delete items

--- session3/linkedlist.py
+++ session3/linkedlist.py
@@ -8,8 +8,11 @@
 class Node:
     # Creating a node
     def __init__(self, item):
-        self.item = item
+        self.data = item
         self.next = None
+
+    def __str__(self):
+        return str(self.data)
 
 
 class LinkedList:
@@ -17,21 +20,42 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    def add(self, new_item):
+        node = Node(new_item)
+        node.next = self.head
+        self.head = node
+        return self.head
 
-if __name__ == '__main__':
+    def __str__(self):
+        output = []
+        curr = self.head
+        while (curr):
+            output.append(curr.data)
+            curr = curr.next
+        return str(output)
 
-    linked_list = LinkedList()
+    def delete(self, item):
+        # for case #1 : if need to delete head then
+        if self.head.data == item:
+            self.head = self.head.next
+            return self.head
 
-    # Assign item values
-    linked_list.head = Node(1)
-    second = Node(2)
-    third = Node(3)
+        # for case #2 : if item have in the list then delete this
+        prev = None
+        curr = self.head
+        while curr and curr.data != item:
+            prev = curr
+            curr = curr.next
+        # for case #3 if no data found in list then check
+        if curr:
+            prev.next = curr.next
+        return self.head
 
-    # Connect nodes
-    linked_list.head.next = second
-    second.next = third
 
-    # Print the linked list item
-    while linked_list.head != None:
-        print(linked_list.head.item, end=" ")
-        linked_list.head = linked_list.head.next
+llist = LinkedList()
+llist.add(10)
+llist.add(4)
+llist.add(16)
+llist.add(19)
+llist.delete(20)
+print(llist)
